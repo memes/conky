f29e89f804020d854d3dc5a0ae9b49517376a7b1
Matthew Emes <memes@matthewemes.com>
2014-05-22 12:40:15 -0700

    Updated to support conky in i3 status bar  - LUA functions added/refactored in i3 preparation  - Fix conky_virtVM to handle whitespace in VM status  - Updated conkyrc to use renamed LUA function  - initial i3 conky configuration

--- conky.lua
+++ conky.lua
@@ -20,9 +20,19 @@ function conky_cpuDisplayName()
    return params['cpuDisplayName'] or ''
 end
 
--- Return the battery object for this system
-function conky_batteryObj()
+-- Return the battery bar for this system
+function conky_batteryBar()
    return string.format('${battery_bar %s}',  params['batteryObj'] or 'BAT0')
+end
+
+-- Return the battery value for this system
+function conky_batteryVal()
+   return string.format('${battery %s}',  params['batteryObj'] or 'BAT0')
+end
+
+-- Return the short battery status for this system
+function conky_batteryStatus()
+   return string.format('${battery_short %s}',  params['batteryObj'] or 'BAT0')
 end
 
 -- Return the cpu temperature for core
@@ -43,6 +53,11 @@ end
 -- Return the cpu bar graph for the core
 function conky_cpuBar(core)
    return string.format('${cpubar cpu%s}', core + 1)
+end
+
+-- Return the cpu percentage for the core
+function conky_cpuVal(core)
+   return string.format('${cpu cpu%s}', core + 1)
 end
 
 -- Return any mounts in /media
@@ -74,9 +89,11 @@ function conky_virtVM(template, context)
       local vm, status = string.match(line, '%s?%d+%s+(%S+)%s+(.+)')
       if (vm) then
 	 if (result) then
-	    result = string.format('%s${%s %s %s}', result, template, vm, status)
+	    result = string.format('%s${%s %s %s}', result, template, vm,
+				   string.gsub(status, " ", "\\ "))
 	 else
-	    result = string.format('${%s %s %s}', template, vm, status)
+	    result = string.format('${%s %s %s}', template, vm,
+				   string.gsub(status, " ", "\\ "))
 	 end
       end
    end
